f85c427351fae00cffb2025e004f1a63274bd883
Quddus Salam <quddussalam555@gmail.com>
2024-01-17 21:58:55 +0700

    Changed conditional to hashmap for faster lookup time, minor refactor in find_mvp

--- src/utils/game/guessr.py
+++ src/utils/game/guessr.py
@@ -6,40 +6,26 @@ from const import (
 )
 
 
-def diff_to_acronym(difficulty):
-    if difficulty == "Easy":
-        return "e"
-    elif difficulty == "Medium":
-        return "m"
-    elif difficulty == "Hard":
-        return "h"
-    elif difficulty == "Very Hard":
-        return "vh"
-    else:
-        raise Exception("Unknown identifier.")
+def diff_to_acronym(difficulty_expanded):
+    difficulty_acronyms = {"Easy": "e", "Medium": "m", "Hard": "h", "Very Hard": "vh"}
+
+    return difficulty_acronyms.get(difficulty_expanded, "e")
 
 
-def diff_to_expanded(difficulty):
-    if difficulty == "e":
-        return "Easy"
-    elif difficulty == "m":
-        return "Medium"
-    elif difficulty == "h":
-        return "Hard"
-    elif difficulty == "vh":
-        return "Very Hard"
-    else:
-        raise Exception("Unknown identifier.")
+def diff_to_expanded(difficulty_acronym):
+    difficulty_expands = {"e": "Easy", "m": "Medium", "h": "Hard", "vh": "Very Hard"}
+
+    return difficulty_expands.get(difficulty_acronym, "Easy")
 
 
 def find_mvp(array):
     count_dict = {}
 
     for item in array:
-        if item in count_dict:
-            count_dict[item] += 1
-        else:
-            count_dict[item] = 1
+        if item not in count_dict:
+            count_dict[item] = 0
+        count_dict[item] += 1
+
     most_common_item = None
     max_count = 0
 
@@ -51,27 +37,18 @@ def find_mvp(array):
     return most_common_item
 
 
-def get_color(difficulty):
-    if difficulty == "Easy":
-        return EASY_COLOR
-    elif difficulty == "Medium":
-        return MEDIUM_COLOR
-    elif difficulty == "Hard":
-        return HARD_COLOR
-    elif difficulty == "Very Hard":
-        return VERY_HARD_COLOR
-    else:
-        raise Exception("Unknown identifier.")
+def get_color(difficulty_expanded):
+    difficulty_colors = {
+        "Easy": EASY_COLOR,
+        "Medium": MEDIUM_COLOR,
+        "Hard": HARD_COLOR,
+        "Very Hard": VERY_HARD_COLOR,
+    }
+
+    return difficulty_colors.get(difficulty_expanded, EASY_COLOR)
 
 
-def get_timeout(difficulty):
-    if difficulty == "Easy":
-        return 10
-    elif difficulty == "Medium":
-        return 15
-    elif difficulty == "Hard":
-        return 20
-    elif difficulty == "Very Hard":
-        return 25
-    else:
-        raise Exception("Unknown identifier.")
+def get_timeout(difficulty_expanded):
+    difficulty_timeouts = {"Easy": 15, "Medium": 20, "Hard": 25, "Very Hard": 30}
+
+    return difficulty_timeouts.get(difficulty_expanded, 15)
